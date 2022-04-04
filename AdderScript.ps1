fd2022616b874b7b733e9337917a4cd1e4f84797
drew895423 <102764496+drew895423@users.noreply.github.com>
2022-04-04 10:47:41 -0400

    Add files via upload

---
+++ AdderScript.ps1
@@ -0,0 +1,17 @@
+﻿Import-Module MicrosoftTeams
+
+Connect-MicrosoftTeams
+
+$userToAdd = Read-Host "Enter username first.last"
+$userToAdd = $userToAdd + "@ticketnetwork.com"
+
+$channelName = Read-Host "Enter channel name ie ITHD365"
+$channelName = Get-Team -DisplayName $channelName
+
+Add-TeamUser -GroupId $channelName.GroupId -User $userToAdd
+
+$subName = Read-Host "Enter sub \ private channel name"
+Write-Host $channelName.GroupId " this id is for " $channelName.DisplayName
+Write-Host $subName
+Write-Host $userToAdd
+Add-TeamChannelUser -GroupId $channelName.GroupId -DisplayName $subName -User $userToAdd
